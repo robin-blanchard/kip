1e3a81b5b6695810061774ca40e8becf8bd72fbd
robin-blanchard <robin.blanchard00@gmail.com>
2023-12-21 00:10:46 +0100

    Add main router

--- kip/endpoints/auth.py
+++ kip/endpoints/auth.py
@@ -4,10 +4,14 @@ from fastapi.routing import APIRouter
 
 from kip.models.auth import User
 from kip.schemas.auth import UserInSchema, UserSchema
+from kip.core.auth import get_password_hash
 
 auth_router = APIRouter()
 
 @auth_router.post("/users/", response_model=UserSchema)
 async def create_user(user_data: UserInSchema) -> Any:
-    user = await User.create(**user_data.model_dump(), hashed_password=user_data.password)
+    user = await User.create(
+        **user_data.model_dump(),
+        hashed_password=get_password_hash(user_data.password)
+    )
     return user
